069166208cd89fbc26f13df7577f20cde0f95ab5
Omar Tharwat <77683787+omartharwat74@users.noreply.github.com>
2024-05-12 20:02:33 +0300

    change color of border

--- Sources/CreateTranslationAi/Extension/Extension + UIVIew.swift
+++ Sources/CreateTranslationAi/Extension/Extension + UIVIew.swift
@@ -27,7 +27,7 @@ extension UIView {
 
 extension UIView {
   func addDashedBorder() {
-      let color = UIColor(red: 87, green: 149, blue: 148, alpha: 1).cgColor
+      let color = UIColor(hexString: "#579594").cgColor
 
     let shapeLayer:CAShapeLayer = CAShapeLayer()
     let frameSize = self.frame.size
@@ -43,5 +43,24 @@ extension UIView {
     shapeLayer.path = UIBezierPath(roundedRect: shapeRect, cornerRadius: 25).cgPath
 
     self.layer.addSublayer(shapeLayer)
+    }
+}
+extension UIColor {
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
     }
 }
