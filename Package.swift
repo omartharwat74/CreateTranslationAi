a56ca4f82aea695ccbd7908da431fadf123a2e69
Omar Tharwat <77683787+omartharwat74@users.noreply.github.com>
2024-05-12 20:38:30 +0300

    test on arabic

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CreateTranslationAi",
-    defaultLocalization: "en",
+    defaultLocalization: "ar",
     platforms: [
         .iOS(.v16),
         .macOS(.v11)
