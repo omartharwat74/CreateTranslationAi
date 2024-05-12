d8aa701f7b0a02377a62d3e563144e6bde514901
Omar Tharwat <77683787+omartharwat74@users.noreply.github.com>
2024-05-12 20:45:27 +0300

    test dash

--- Sources/CreateTranslationAi/Extension/Extension + UIVIew.swift
+++ Sources/CreateTranslationAi/Extension/Extension + UIVIew.swift
@@ -27,21 +27,14 @@ public func SCImage(named name: String) -> UIImage? {
 
 extension UIView {
     func addDashedBorder() {
-        let color = UIColor(red: 0.341, green: 0.584, blue: 0.58, alpha: 1).cgColor
         
         let shapeLayer = CAShapeLayer()
-        let frameSize = self.frame.size
-        let shapeRect = CGRect(x: 0, y: 0, width: frameSize.width, height: frameSize.height)
-        
-        shapeLayer.bounds = shapeRect
-        shapeLayer.position = CGPoint(x: frameSize.width/2, y: frameSize.height/2)
         shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.strokeColor = color
+        shapeLayer.strokeColor = UIColor(red: 0.341, green: 0.584, blue: 0.58, alpha: 1).cgColor
         shapeLayer.lineWidth = 0.5
         shapeLayer.lineJoin = .round
         shapeLayer.lineDashPattern = [10, 5]
-        let largerRect = shapeRect.insetBy(dx: -shapeLayer.lineWidth / 2, dy: -shapeLayer.lineWidth / 2)
-        shapeLayer.path = UIBezierPath(roundedRect: largerRect, cornerRadius: 25).cgPath
+        shapeLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: 25).cgPath
         
         self.layer.addSublayer(shapeLayer)
     }
