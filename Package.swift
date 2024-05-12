89a205c5114c37c6291bcfd8e52952f34ee9817e
Omar Tharwat <77683787+omartharwat74@users.noreply.github.com>
2024-05-12 17:29:59 +0300

    test

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         .target(
             name: "CreateTranslationAi",
-//            dependencies: ["SwiftyMenu"],
+            dependencies: ["SwiftyMenu"],
             resources: [
                 .copy("Fonts/DINNextLTArabic-Bold-4.ttf"),
                 .copy("DINNextLTArabic-Medium-4.ttf"),
