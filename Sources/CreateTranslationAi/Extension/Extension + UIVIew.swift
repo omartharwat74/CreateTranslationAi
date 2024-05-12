54c41abc7f3c3f82a46576818b46ee5a780511b9
Omar Tharwat <77683787+omartharwat74@users.noreply.github.com>
2024-05-12 20:58:01 +0300

    test again

--- Sources/CreateTranslationAi/Extension/Extension + UIVIew.swift
+++ Sources/CreateTranslationAi/Extension/Extension + UIVIew.swift
@@ -47,13 +47,19 @@ extension UIView {
         shapeLayer.lineDashPattern = [10, 5]
         
         // Define the inset to adjust the position of the border
-        let inset: CGFloat = 0.25
+        let inset: CGFloat = 0.5
         let rect = bounds.insetBy(dx: inset, dy: inset)
         
+        // Use the rounded rect of the adjusted size
         shapeLayer.path = UIBezierPath(roundedRect: rect, cornerRadius: 25).cgPath
+        
+        // Ensure the shape layer is clipped to the bounds of the view
+        shapeLayer.bounds = bounds
+        shapeLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
         
         self.layer.addSublayer(shapeLayer)
     }
+
 
 }
 
