71e8f0da69ae776333a3a80cba1ac99c250a5dfc
Omar Tharwat <77683787+omartharwat74@users.noreply.github.com>
2024-05-13 21:50:07 +0300

    test

--- Sources/CreateTranslationAi/Extension/FloatingTextField.swift
+++ Sources/CreateTranslationAi/Extension/FloatingTextField.swift
@@ -162,7 +162,7 @@ class DropDownTextField: MGFloatingTextField {
         selectedImageView.frame = containerView.frame
         rightView = containerView
         rightViewMode = .always
-        self.padding = UIEdgeInsets(top: 0, left: -50, bottom: 0, right: 50)
+        self.padding = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
     private func setupDesign() {
