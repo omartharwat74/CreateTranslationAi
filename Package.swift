e9aa38cba8544b228a09c4ee2108255e1656038c
Omar Tharwat <77683787+omartharwat74@users.noreply.github.com>
2024-05-18 19:27:40 +0300

    test

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
         .target(
             name: "CreateTranslationAi",
             resources: [
+                .copy("TranslateCell.xib"),
                 .copy("Fonts/DINNextLTArabic-Bold-4.ttf"),
                 .copy("DINNextLTArabic-Medium-4.ttf"),
                 .copy("DINNextLTArabic-Regular-3.ttf"),
